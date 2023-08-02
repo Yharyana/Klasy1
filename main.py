4039e9cc9336018c362ddf676bb90524b959c65f
Jakub Yharyana <kubazbroinsk@gmail.com>
2023-08-02 12:55:15 +0200

    dziala

--- main.py
+++ main.py
@@ -1,16 +1,44 @@
-# This is a sample Python script.
+#class product:
+#    pass
+#ziemniak=product()
+#ogurek=product()
+#chlep=product()
+#print(type(ziemniak))
+#lista_produktow={
+ #   ziemniak:type(ziemniak)
+#}
+#print(lista_produktow)
+class Product:
+    def __init__(self,name,name_of_category,price):
+        self.name=name
+        self.name_of_category=name_of_category
+        self.price=price
+class Order:
+    def __init__(self,name,surrname,list=False):
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+        self.surrname = surrname
+        self.name = name
+        if list==False:
+            list=[]
+        self.list = list
+        whole_price=sum(list)
+        self.whole_price = whole_price
+class Apple:
+    def __init__(self,gatunek,rozmiar,price_per_kg):
+        self.rozmiar=rozmiar
+        self.price_per_kg = price_per_kg
+        self.gatunek = gatunek
 
+class Potato:
+    def __init__(self,gatunek,rozmiar,price_per_kg):
+        self.rozmiar=rozmiar
+        self.price_per_kg = price_per_kg
+        self.gatunek = gatunek
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+apple1=Apple("sweet",",medium",4.20)
+potato1=Potato("sweet",",medium",4.20)
+produkt1=Product("japka","warzywa",12)
+order1=Order("jakub","dec",[12,3,4])
 
+def wypisanie(Order):
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
